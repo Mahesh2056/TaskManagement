f6c2ae6c3f222239d5a3403a125170a9b14cb9b6
Mahesh2056 <maheschy2323@gmail.com>
2023-08-08 02:18:45 +0545

    some changes

--- client/src/pages/login/index.js
+++ client/src/pages/login/index.js
@@ -1,47 +1,49 @@
-import React from 'react';
-import { Formik, Form, Field } from 'formik';
-import * as Yup from 'yup';
+import React from "react";
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
 
 const SigninSchema = Yup.object().shape({
-  email: Yup.string().email('Invalid email').required('Required'),
+  email: Yup.string().email("Invalid email").required("Required"),
   password: Yup.string()
-    .min(2, 'Too Short!')
-    .max(50, 'Too Long!')
-    .required('Required'),
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
 });
 
- const Login = () => (
-  <div>
-    <h1>Sign In</h1>
-    <Formik
-      initialValues={{
-        email: '',
-        password: '',
-      }}
-      validationSchema={SigninSchema}
-      onSubmit={values => {
-        // same shape as initial values
-        console.log(values);
-      }}
-    >
-      {({ errors, touched }) => (
-        <Form>
-          <div>
-          <Field placeholder="email" name="email" type="email" />
-          {errors.email && touched.email ? <div>{errors.email}</div> : null}
-          </div>
-          <div>
-          <Field placeholder="password" name="password" />
-          {errors.password && touched.password ? (
-            <div>{errors.password}</div>
-          ) : null}
-          </div>
-          <div>
-          <button type="login">Log In</button>
-          </div>
-        </Form>
-      )}
-    </Formik>
-  </div>
+const Login = () => (
+  <center>
+    <div>
+      <h1>Sign In</h1>
+      <Formik
+        initialValues={{
+          email: "",
+          password: "",
+        }}
+        validationSchema={SigninSchema}
+        onSubmit={(values) => {
+          // same shape as initial values
+          console.log(values);
+        }}
+      >
+        {({ errors, touched }) => (
+          <Form>
+            <div>
+              <Field placeholder="email" name="email" type="email" />
+              {errors.email && touched.email ? <div>{errors.email}</div> : null}
+            </div>
+            <div>
+              <Field placeholder="password" name="password" />
+              {errors.password && touched.password ? (
+                <div>{errors.password}</div>
+              ) : null}
+            </div>
+            <div>
+              <button type="login">Log In</button>
+            </div>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  </center>
 );
-export default Login
+export default Login;
